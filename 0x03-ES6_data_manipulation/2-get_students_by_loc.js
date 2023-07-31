e9ad54c02d7fb2022774bf1b8b25ae4bf8a574b4
Mac <mac@Macs-MacBook-Pro.local>
2023-07-31 21:24:00 +0100

    filter array by city

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+// Returns an array of Student objects who are located in a specific city.
+export default function getStudentsByLocation(arrOfStudentIds, city) {
+  const arr = [];
+  if (Array.isArray(arrOfStudentIds) && typeof city === 'string') {
+    return arrOfStudentIds.filter((student) => student.location === city);
+  }
+  return arr;
+}
