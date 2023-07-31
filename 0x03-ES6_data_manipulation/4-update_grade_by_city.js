7412389f72089cd2b1ba2fa9594a06ae95150be4
Mac <mac@Macs-MacBook-Pro.local>
2023-07-31 21:40:28 +0100

    array find

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+// Returns an array of students for a specific city with their grade
+export default function updateStudentGradeByCity(arrOfStudentObjs, city, newGrades) {
+  if (Array.isArray(arrOfStudentObjs) && typeof city === 'string' && typeof newGrades === 'object') {
+    const filtStudents = arrOfStudentObjs.filter((obj) => obj.location === city);
+
+    filtStudents.forEach((studentObj) => {
+      const objFound = newGrades.find((obj) => obj.studentId === studentObj.id);
+      if (objFound) { studentObj.grade = objFound.grade; } else { studentObj.grade = 'N/A'; }
+    });
+
+    return filtStudents;
+  }
+  return [];
+}
