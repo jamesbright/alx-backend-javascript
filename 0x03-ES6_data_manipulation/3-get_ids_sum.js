34a60413067c8d29197ef85c04b3d4d5e312fe03
Mac <mac@Macs-MacBook-Pro.local>
2023-07-31 21:25:55 +0100

    sum ids with reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+// Sums up all of the id
+export default function getStudentIdsSum(arrOfStudentObjs) {
+  if (Array.isArray(arrOfStudentObjs)) {
+    return arrOfStudentObjs.reduce((acc, currObj) => acc + currObj.id, 0);
+  }
+  return 0;
+}
