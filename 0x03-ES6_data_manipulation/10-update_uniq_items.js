94f95dbcf9876c01c03c9ee537842aec43ae9ab7
Mac <mac@Macs-MacBook-Pro.local>
2023-08-01 08:56:54 +0100

    add updateUniqueItems

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+// Change items whose qty == 1 to 100
+export default function updateUniqueItems(someMap) {
+  try {
+    return [...someMap.entries()]
+      .filter((keyValArr) => keyValArr[1] === 1)
+      .map((keyValArr) => someMap.set(keyValArr[0], 100));
+  } catch (e) {
+    throw Error('Cannot process');
+  }
+}
